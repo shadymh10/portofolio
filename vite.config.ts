b68149070db806b54b3c339d9cf3d577fd347ae4
shady <shadymhmd2004@gmail.com>
2025-09-12 22:52:21 +0300

    Update project files and change repo link

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portofolio/",   // 👈 ده مهم عشان GitHub Pages يشتغل
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
