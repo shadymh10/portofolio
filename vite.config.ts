5f42d5863da9b37a374c0da84059409751056a68
shady <shadymhmd2004@gmail.com>
2025-09-12 23:52:26 +0300

    Update config and package scripts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/portofolio/",   // 👈 ده مهم عشان GitHub Pages يشتغل
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: command === "serve" ? "/" : "/portofolio/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === "development" && componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
